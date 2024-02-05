69100a30a9f6c057bb311ee6bb5127109e8d0dc6
João Veloso <141861100+joaoVelosoT@users.noreply.github.com>
2024-02-05 16:46:11 -0300

    aula03

---
+++ aula03/script.js
@@ -0,0 +1,25 @@
+// const nome = document.querySelector("#nome");
+// const button = document.querySelector("button");
+// const lista = document.querySelector(".lista");
+
+// const allPs = document.querySelectorAll("p.select")
+
+// allPs.forEach(item => console.log(item.innerHTML));
+
+
+// console.log(allPs.length);
+
+// button.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     const inputValue = nome.value;
+//     const templateHTML =`<li>${inputValue}</li>`;
+
+//     lista.innerHTML += templateHTML;
+
+//     nome.value = "";
+// })
+
+const itens = document.querySelectorAll("h1, p, input, button, ul");
+
+console.log(itens);
+console.log("oi")   
---
+++ aula03/script01.js
@@ -0,0 +1,19 @@
+const h1 = document.querySelectorAll("h1");
+const p = document.querySelectorAll("p");
+const input = document.querySelectorAll("input");
+const button = document.querySelectorAll("button");
+const ul = document.querySelectorAll("ul");
+
+
+h1.forEach(item => console.log(item.innerHTML));
+p.forEach(item => console.log(item.innerHTML));
+input.forEach(item => console.log(item.innerHTML));
+button.forEach(item => console.log(item.innerHTML));
+ul.forEach(item => console.log(item.innerHTML));
+
+
+console.log(h1.length);
+console.log(p.length);
+console.log(input.length);
+console.log(button.length);
+console.log(ul.length);
